d6d5d6da3fd0e5dcd903f58e180bedaa1cd4749e
TSraj <tanvirraj475@gmail.com>
2026-06-21 19:24:27 +0200

    added run folder

---
+++ run/job1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=j1
+#SBATCH --output=logs/out_%j.out
+#SBATCH --error=logs/err_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --partition=v100
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=8
+#SBATCH --export=NONE
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tanver.s.raj@fau.de
+
+# -----------------------------------------------------------
+# Environment
+# -----------------------------------------------------------
+
+unset SLURM_EXPORT_ENV
+
+module purge
+module load python/3.12-conda
+source venv/bin/activate
+
+export http_proxy=http://proxy:80
+export https_proxy=http://proxy:80 
+
+echo "job1.sh this is job1.sh"
